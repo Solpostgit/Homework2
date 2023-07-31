ebb08a3eff514eda88c9ec3e2d89ffc1e0208244
Solpostgit <begemot_off@mail.ru>
2023-07-31 22:44:51 +0300

    task 1-3, 4-5, 6-7 and 8 completed

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        task1();
-        task2();
-        task3();
+        task1_2_3();
+        task4_5();
+        task6_7();
+        task8();
     }
-    public static void task1() {
+    public static void task1_2_3() {
         System.out.println("task 1");
         var dog = 8.0;
         var cat = 3.6;
@@ -31,7 +32,7 @@ public class Main {
         System.out.println(paper);
         System.out.println();
     }
-    public static void task2() {
+    public static void task4_5() {
         System.out.println("task 4");
         var friend = 19;
         System.out.println(friend);
@@ -51,8 +52,33 @@ public class Main {
         System.out.println(frog);
         System.out.println();
     }
-    public static void task3() {
+    public static void task6_7() {
+        System.out.println("task 6");
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+        var weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Общий вес боксёров " + totalWeight + " кг!");
+        System.out.println("Разница в весе боксёров " + weightDifference + " кг!");
 
+        System.out.println("task 7");
+        System.out.println("Вычитание из большего веса меньшего.");
+        weightDifference = secondBoxer - firstBoxer;
+        System.out.println("Разница в весе боксёров " + weightDifference + " кг!");
+        System.out.println("C помощью функции остаток от деления");
+        weightDifference = secondBoxer % firstBoxer;
+        System.out.println("Разница в весе боксёров " + weightDifference + " кг!");
+    }
+    public static void task8() {
+        System.out.println();
+        System.out.println("task 8");
+        var totalWorkingHours = 640;
+        var job = 8;
+        var workers = totalWorkingHours / job;
+        System.out.println("Всего работников в компании - " + workers + " человек");
+        workers = workers + 94;
+        totalWorkingHours = workers * job;
+        System.out.println("Если в компании работает " + workers + " человек, то всего " + totalWorkingHours + " часов работы может быть поделено между сотрудниками");
     }
 
 }
