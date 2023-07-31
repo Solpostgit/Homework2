7cf19f8e4a6da7f2228ce534688e22f60370c7f9
Solpostgit <begemot_off@mail.ru>
2023-07-31 22:00:03 +0300

    task 1-3 and 4-5 completed

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         task3();
     }
     public static void task1() {
+        System.out.println("task 1");
         var dog = 8.0;
         var cat = 3.6;
         var paper = 763789;
@@ -13,6 +14,7 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
 
+        System.out.println("task 2");
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
@@ -20,16 +22,34 @@ public class Main {
         System.out.println(cat);
         System.out.println(paper);
 
+        System.out.println("task 3");
         dog = dog - 3.5;
         cat = cat - 1.6;
         paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
-
+        System.out.println();
     }
     public static void task2() {
+        System.out.println("task 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
 
+        System.out.println("task 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println();
     }
     public static void task3() {
 
